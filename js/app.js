d9dba969ef9cfa344466557375d4f634d71a7c6c
Wills Bithrey <wills@potatolondon.com>
2012-12-03 06:43:29 +0000

    Implementing basic calibration

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 /*global $, google, console */
+/*
+	Lat = y axis
+	Lng = x axis
+
+*/
 (function () {
 	"use strict";
 	var MAP_CONFIG = {
@@ -7,6 +12,63 @@
 		tilePath: 'http://f12012pf.willsbithrey.com/tiles/melbourne/',
 		maxZoom: 5
 	};
+
+	var CALIBRATING = false,
+		CIRCUITS = {
+			MELBOURNE: {
+				calibrationPoints: [
+					{
+						simed: {
+							x: -104.216,
+							y: -388.953
+						},
+						gmaps: {
+							x: 120.625,
+							y: 155.53125
+						}
+					},
+					{
+						simed: {
+							x: -191.783,
+							y: 920.459
+						},
+						gmaps: {
+							x: 114.25,
+							y: 61.3125
+						}
+					},
+				],
+				scale: 1,
+				tilePath: 'http://f12012pf.willsbithrey.com/tiles/melbourne/'
+			}
+		};
+
+	function calibrate(circuitName) {
+		var circuit = CIRCUITS[circuitName];
+		var points = circuit.calibrationPoints;
+		console.log('Running calibration for ' + circuitName.toLowerCase());
+
+		var simedDeltaX = Math.abs(points[0].simed.x - points[1].simed.x);
+		var simedDeltaY = Math.abs(points[0].simed.y - points[1].simed.y);
+
+		var gmapsDeltaX = Math.abs(points[0].gmaps.x - points[1].gmaps.x);
+		var gmapsDeltaY = Math.abs(points[0].gmaps.y - points[1].gmaps.y);
+
+		var scaleX = (simedDeltaX > gmapsDeltaX) ? simedDeltaX / gmapsDeltaX : gmapsDeltaX / simedDeltaX;
+		var scaleY = (simedDeltaY > gmapsDeltaY) ? simedDeltaY / gmapsDeltaY : gmapsDeltaY / simedDeltaY;
+		var scaleAvg = (scaleX + scaleY) / 2;
+
+		console.log('3dSimEd Delta ('+ simedDeltaX + ', ' + simedDeltaY + ')');
+		console.log('GMaps Delta (' + gmapsDeltaX + ', ' + gmapsDeltaY + ')');
+
+		console.log('scaleX:   ' + scaleX);
+		console.log('scaleY:   ' + scaleY);
+		console.log('scaleAvg: ' + scaleAvg);
+
+		console.log('Calibrated');
+
+		circuit.scale = scaleAvg;
+	}
 
 	/*
 	 * Helper function which normalizes the coords so that tiles can repeat across the X-axis (horizontally) like the standard Google map tiles.
@@ -42,6 +104,20 @@
 		};
 	}
 
+	function enableCalibration() {
+		CALIBRATING = true;
+		$('.circuit_calibrate').removeClass('btn-inverse').addClass('btn-info').text('Calibrating');
+		$('.calibrating-alert').css('display', 'block').fadeIn();
+	}
+
+	function disableCalibration() {
+		CALIBRATING = false;
+		$('.circuit_calibrate').removeClass('btn-info').addClass('btn-inverse').text('Calibrate');
+		$('.calibrating-alert').fadeOut(function() {
+			$(this).css('display', 'none');
+		});
+	}
+
 	// Define our custom map type
 	var circuitMapType = new google.maps.ImageMapType({
 		getTileUrl: function (coord, zoom) {
@@ -60,6 +136,7 @@
 	// Basic options for our map
 	var circuitMapOptions = {
 		center: new google.maps.LatLng(0, 0),
+		draggableCursor: 'crosshair',
 		zoom: 2,
 		minZoom: 0,
 		streetViewControl: false,
@@ -77,48 +154,51 @@
 	map.mapTypes.set('circuit_map', circuitMapType);
 	map.setMapTypeId('circuit_map');
 
+	google.maps.event.addListener(map, "rightclick", function(event) {
+		if (CALIBRATING) {
+			var knownCoord = window.prompt('Enter the clicked points coordinate');
+			if (!knownCoord) {
+				return;
+			} else {
+				knownCoord = knownCoord.split(' ');
+			}
 
+			var knownX = knownCoord[0];
+			var knownY = knownCoord[1];
 
+			var gmapsX = event.ga.x * CIRCUITS.MELBOURNE.scale;
+			var gmapsY = event.ga.y * CIRCUITS.MELBOURNE.scale;
 
+			var offsetX = (knownX > gmapsX) ? knownX - gmapsX : gmapsX - knownX;
+			var offsetY = (knownY > gmapsY) ? knownY - gmapsY : gmapsY - knownY;
 
+			var offsetAvg = (Math.abs(offsetX) + Math.abs(offsetY)) / 2;
 
+			console.log('offsetX:   ' + offsetX);
+			console.log('offsetY:   ' + offsetY);
+			console.log('offsetAvg: ' + offsetAvg);
 
+			CIRCUITS.MELBOURNE.offset = offsetX && offsetY > 0 ? offsetAvg : -offsetAvg;
 
+			disableCalibration();
+		} else {
+			var x = (event.ga.x * CIRCUITS.MELBOURNE.scale) + CIRCUITS.MELBOURNE.offset;
+			var y = (event.ga.y * CIRCUITS.MELBOURNE.scale) + CIRCUITS.MELBOURNE.offset;
+			console.log("x " + x + " y " + y);
+		}
+	});
 
+	// Calibrate the selected circuit
+	calibrate('MELBOURNE');
 
-
-
-
-	// // Create our custom map type
-	// function CoordMapType(tileSize) {
-	// 	this.tileSize = tileSize;
-	// }
-
-	// CoordMapType.prototype.getTile = function (coord, zoom, ownerDocument) {
-	// 	var div = ownerDocument.createElement('div');
-	// 	// div.innerHTML = coord;
-	// 	// div.style.width = this.tileSize.width + 'px';
-	// 	// div.style.height = this.tileSize.height + 'px';
-	// 	// div.style.fontSize = '10';
-	// 	// div.style.borderStyle = 'solid';
-	// 	// div.style.borderWidth = '1px';
-	// 	// div.style.borderColor = '#AAAAAA';
-	// 	return div;
-	// };
-
-	// function initialize() {
-	// 	var albertPark = new google.maps.LatLng(-37.846799, 144.972912),
-	// 		map,
-	// 		mapOptions = {
-	// 			center: albertPark,
-	// 			zoom: 14,
-	// 			mapTypeId: google.maps.MapTypeId.ROADMAP
-	// 		},
-	// 		$map = $('.map-google-map-container');
-
-	// 	map = new google.maps.Map($map[0], mapOptions);
-	// 	map.overlayMapTypes.insertAt(0, new CoordMapType(new google.maps.Size(256, 256)));
-	// }
+	// Bind UI
+	$('.circuit_calibrate').on('click', function(evnt) {
+		if (CALIBRATING) {
+			disableCalibration();
+		} else {
+			enableCalibration();
+		}
+	});
 
 	$(document).ready(function () {
 		console.log('Ready');
