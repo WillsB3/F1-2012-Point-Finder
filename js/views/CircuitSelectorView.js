d61e5e0a0483be1e19bcd41cc1ca27f84a32e845
Wills Bithrey <wills@potatolondon.com>
2012-12-09 16:02:40 +0000

    Adding start of search field

--- js/views/CircuitSelectorView.js
+++ js/views/CircuitSelectorView.js
@@ -1,4 +1,4 @@
-/*global $, Backbone, _, f1 */
+/*global $, Backbone, _, f1, google */
 (function () {
 	"use strict";
 	f1.views.CircuitSelector = Backbone.View.extend({
@@ -6,15 +6,52 @@
 		className: "circuit-selector",
 		template: $('#circuit-selector-template').html(),
 
+		elements: {
+			$searchField: null
+		},
+
+		events: {
+			'focus .search-field': 'expandSearchField',
+			'blur .search-field': 'contractSearchField',
+			'submit .circuit-selector-search': 'preventDefaultFormBehaviour'
+		},
+
 		initalize: function () {
 			f1.log('CircuitSelector:initalize');
+
+			_.bindAll(this, 'contractSearchField', 'expandSearchField');
 
 			return this;
 		},
 
+		contractSearchField: function (evnt) {
+			this.elements.$searchField.css({
+				width: 'auto'
+			});
+		},
+
+		expandSearchField: function (evnt) {
+			this.elements.$searchField.css({
+				width: '400px'
+			});
+		},
+
+		preventDefaultFormBehaviour: function (evnt) {
+			// Prevent the form submitting
+			evnt.preventDefault();
+		},
+
 		render: function () {
 			f1.log('CircuitSelector:render');
+
+			// Render the template
 			this.$el.html(f1.template(this.template, { circuits: f1.circuits }));
+
+			// Cache required elements
+			this.elements.$searchField = this.$el.find('input.search-field');
+
+			// Initialise the search box
+			this.searchBox = new google.maps.places.SearchBox(this.elements.$searchField[0]);
 
 			return this;
 		},
