1087e94b89fd3559a39718ea9d4c3d24d4756de4
Wills Bithrey <wills@potatolondon.com>
2012-12-14 21:17:06 +0000

    Creating stubby initialise and render for TriggerMapperPage

--- js/views/Pages/TriggerMapperPage.js
+++ js/views/Pages/TriggerMapperPage.js
@@ -2,6 +2,20 @@
 (function () {
 	"use strict";
 	f1.pages.TriggerMapperPage = f1.pages.BaseCircuitPageView.extend({
+		initialize: function () {
+			f1.log('TriggerMapperPage:initalize');
+			f1.pages.BaseCircuitPageView.prototype.initialize.apply(this);
 
+			return this;
+		},
+
+		render: function () {
+			f1.log('TriggerMapperPage:render');
+			f1.pages.BaseCircuitPageView.prototype.render.apply(this);
+
+
+
+			return this;
+		}
 	});
 }());
