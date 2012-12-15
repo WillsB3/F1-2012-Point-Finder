be3b9ab63801a77f9910c6e072f5fab259970452
Wills Bithrey <wills@potatolondon.com>
2012-12-15 09:30:20 +0000

    Adding drawing!

--- js/views/Pages/TriggerMapperPage.js
+++ js/views/Pages/TriggerMapperPage.js
@@ -2,6 +2,25 @@
 (function () {
 	"use strict";
 	f1.pages.TriggerMapperPage = f1.pages.BaseCircuitPageView.extend({
+		drawingManagerOptions: {
+			drawingMode: google.maps.drawing.OverlayType.POLYGON,
+			drawingControl: true,
+			drawingControlOptions: {
+				position: google.maps.ControlPosition.BOTTOM_CENTER,
+				drawingModes: [
+					google.maps.drawing.OverlayType.POLYGON
+				]
+			},
+			polygonOptions: {
+				editable: true,
+				clickable: false,
+				fillColor: '#ff0600',
+				fillOpacity: 0.5,
+				strokeWeight: 2,
+				zIndex: 1
+			}
+		},
+
 		initialize: function () {
 			f1.log('TriggerMapperPage:initalize');
 			f1.pages.BaseCircuitPageView.prototype.initialize.apply(this);
@@ -14,7 +33,7 @@
 			f1.pages.BaseCircuitPageView.prototype.render.apply(this);
 
 			// Create the drawing manager
-			this.drawingManager = new google.maps.drawing.DrawingManager();
+			this.drawingManager = new google.maps.drawing.DrawingManager(this.drawingManagerOptions);
 			this.drawingManager.setMap(this.map);
 
 			return this;
