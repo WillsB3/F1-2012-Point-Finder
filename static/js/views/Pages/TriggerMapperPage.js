4506565913b5886112cbb29cd90f53242c68cc51
Wills Bithrey <wills@willsbithrey.com>
2013-02-23 10:05:46 +0000

    Adding basic delete point functionality to Trigger Mapper Page

--- static/js/views/Pages/TriggerMapperPage.js
+++ static/js/views/Pages/TriggerMapperPage.js
@@ -22,17 +22,50 @@
 		},
 
 		drawingManager: null,
+		polygons: null,
 
 		initialize: function () {
 			f1.log('TriggerMapperPage:initalize');
 			f1.pages.BaseCircuitPageView.prototype.initialize.apply(this);
 
+			_.bindAll(this, 'onOverlayComplete');
+
+			this.polygons = [];
+
 			return this;
+		},
+
+		bindEvents: function () {
+			f1.pages.BaseCircuitPageView.prototype.bindEvents.apply(this, arguments);
+		},
+
+		unbindEvents: function () {
+			f1.pages.BaseCircuitPageView.prototype.unbindEvents.apply(this, arguments);
+		},
+
+		deleteNode: function (mev) {
+			if (mev.vertex && this.getPath().getLength() > 3) {
+				this.getPath().removeAt(mev.vertex);
+			}
+		},
+
+		onOverlayComplete: function (overlay, evnt) {
+			// Only accept the polygon if it has 3 or more points.
+			if (evnt.overlay.getPath().getLength() >= 3) {
+				if (evnt.type === google.maps.drawing.OverlayType.POLYGON) {
+					this.polygons.push(evnt.overlay);
+
+					// Setup deletion of points
+					google.maps.event.addListener(evnt.overlay, 'click', this.deleteNode);
+				}
+			}
 		},
 
 		render: function () {
 			f1.log('TriggerMapperPage:render');
 			f1.pages.BaseCircuitPageView.prototype.render.apply(this);
+
+			var self = this;
 
 			// Create the drawing manager
 			this.drawingManager = new google.maps.drawing.DrawingManager(this.drawingManagerOptions);
@@ -42,7 +75,16 @@
 			// control before they can begin drawing.
 			this.drawingManager.setDrawingMode(null);
 
+			// Setup tracking of polygons
+			google.maps.event.addListener(this.drawingManager, 'overlaycomplete', function () {
+				self.onOverlayComplete.apply(self, [this].concat(Array.prototype.slice.call(arguments)));
+			});
+
 			return this;
+		},
+
+		close: function () {
+			f1.pages.BaseCircuitPageView.close.apply(this, arguments);
 		}
 	});
 }());
